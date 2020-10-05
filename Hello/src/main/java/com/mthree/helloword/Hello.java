8e8235afad80b1bb7aa7dbac486cc70295beeef6
Francine Faida <faidafr@gmail.com>
2020-10-05 17:23:06 -0400

    Hello World in NetBeans

---
+++ Hello/src/main/java/com/mthree/helloword/Hello.java
@@ -0,0 +1,15 @@
+/**
+ *@author Francine Faida
+ *email: faidafr@gmail.com
+ *date: october5,2020
+ *purpose: My first Hello World program in an IDE
+ */
+
+// Use YOUR Group Id value here if it is different from com.mthree
+package com.mthree.helloworld; 
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, Francine Faida");
+    }
+}
